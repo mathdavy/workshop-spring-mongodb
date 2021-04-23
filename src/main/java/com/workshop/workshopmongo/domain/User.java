bfe321090f528bda89493220d54e42ff64221f07
mathdavy <matheusuzumaki26@gmail.com>
2021-04-23 16:51:16 -0300

    referencing user posts

--- src/main/java/com/workshop/workshopmongo/domain/User.java
+++ src/main/java/com/workshop/workshopmongo/domain/User.java
@@ -2,9 +2,12 @@ package com.workshop.workshopmongo.domain;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -17,6 +20,14 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String id, String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
 
     public User(String name, String email) {
         this.name = name;
