b12d1c3090a3d4e069745c7aa983138afce88a68
mathdavy <matheusuzumaki26@gmail.com>
2021-04-22 23:29:47 -0300

    Created entity post

---
+++ src/main/java/com/workshop/workshopmongo/domain/Post.java
@@ -0,0 +1,23 @@
+package com.workshop.workshopmongo.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Document
+public class Post implements Serializable {
+    @Id
+    private String id;
+    private Date date;
+    private String title;
+    private String body;
+    private User author;
+}
---
+++ src/main/java/com/workshop/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.workshop.workshopmongo.repository;
+
+import com.workshop.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
